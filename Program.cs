632a162604360316bff2cbb9babcf72c491b5aa2
Matricarin <matricarin@gmail.com>
2023-03-15 22:12:39 +0500

    Add input data

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿Console.SetWindowSize(80, 24);
-Console.SetBufferSize(80, 48);
-
-
-Console.WriteLine("*** TABLE ***");
-
+﻿Console.WriteLine("*** TABLE ***");
+int size = 0;
+bool breaker = false;
+string? text = null;
+char border = '+';
 do{
-    bool breaker = false;
     Console.WriteLine("Enter the dimension of the table: ");
-}
+    breaker = int.TryParse(Console.ReadLine(), out size);
+    if(!breaker)
+    {
+        Console.WriteLine("Incorrect input!");
+    }
+    else if(size < 6 && size > 1)
+    {
+        Console.WriteLine("Success!");
+    }
+    else
+    {
+        Console.WriteLine("Entered incorrect size. Size of a table must from 1 to 6.");
+    }
+}while(size < 1 || size > 6);
 
-Console.ReadKey();
+while(breaker)
+{
+    Console.WriteLine("Enter the any text: ");
+    text = Console.ReadLine();
+    if(text == null || text == " " || text == "")
+    {
+        Console.WriteLine("Entered incorrect line.");
+        breaker = true;
+    }
+    else
+    {
+        Console.WriteLine("Success");
+        breaker = false;
+    }
+}
